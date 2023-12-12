0c6979fc5397bd9de2031594082100877df3a2a5
Rishabh Jain <rick.jain02@gmail.com>
2023-12-12 07:17:32 -0500

    reorganization and implementing React

--- client/src/loginHandler.js
+++ client/src/loginHandler.js
@@ -1,25 +1,6 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     document.getElementById('login_button').addEventListener('click', function() {
-//         window.location.href = '/login';
-//     })
-// })
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class LoginButton extends React.Component {
-    handleLogin = () => {
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('login_button').addEventListener('click', function() {
         window.location.href = '/login';
-    }
+    })
+})
 
-    render() {
-        return (
-            <div>
-                <h1>Click to login</h1>
-                <button id="login_button" onClick={this.handleLogin}>Login</button>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<LoginButton />, document.getElementById('root'));
